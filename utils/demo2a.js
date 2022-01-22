4f1608e4b4c7a267c571044b8e6ec9b131079d3f
Sher Chowdhury <sher.chowdhury@ibm.com>
2022-01-22 21:14:37 +0000

    wip

--- utils/demo2a.js
+++ utils/demo2a.js
@@ -1,10 +1,6 @@
 'use strict'
 
-function loudPrint(msg){
+module.exports = function loudPrint(msg){
 	console.log(msg)
 } 
 
-// notice here we can use "module.exports" to export the function as a different name to the one we've defined in the file. 
-module.exports = {
-	print: loudPrint
-}
